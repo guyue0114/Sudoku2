dcc451fe4440945c673a82abafb37382251f8f9a
weitaohu <824784596@qq.com>
2025-04-23 14:38:19 -0700

    isValid() has been completed

--- SudokuBoard.java
+++ SudokuBoard.java
@@ -2,38 +2,48 @@ import java.util.*;
 import java.io.*;
 
 public class SudokuBoard {
-   private int[][] board;
-   
-   public SudokuBoard(String filename) {
-      board = new int[9][9];
-      try {
-         Scanner file = new Scanner(new File(filename));
-         
-         for(int r = 0; r < 9; r++) {
-            String line = file.nextLine();
-            for(int c = 0; c < 9; c++) {
-               Scanner lineScan = new Scanner(line);
-               char num = lineScan.next().charAt(c);
-               if(num == '.') {
-                  board[r][c] = 0;
-               }else {
-                  board[r][c] = num;
-               }
+    private int[][] board;
+
+    public SudokuBoard(String filename) {
+        board = new int[9][9];
+        try {
+            Scanner file = new Scanner(new File(filename));
+            for (int r = 0; r < 9; r++) {
+                String line = file.nextLine();
+                for (int c = 0; c < 9; c++) {
+                    char ch = line.charAt(c);
+                    if (ch == '.' || ch == '0') {
+                        board[r][c] = 0;
+                    } else {
+                        board[r][c] = Character.getNumericValue(ch);
+                    }
+                }
             }
-         }
-         
-      } catch(FileNotFoundException e) {
-         System.out.println("Cannot load: " + filename);
-      } catch(InputMismatchException e) {
-         System.out.println(filename + " does not meet format expectations.");
-      }
-   }
-   
-   public boolean isValid() {
-      return isValidData() && rowsValid() && columnsValid() && miniSquaresValid();
-   }
-   
-   private boolean isValidData() {
+        } catch (FileNotFoundException e) {
+            System.out.println("Cannot load: " + filename);
+        } catch (Exception e) {
+            System.out.println(filename + " does not meet format expectations.");
+        }
+    }
+
+    public String toString() {
+        StringBuilder build = new StringBuilder(" -----------------\n");
+        for (int r = 0; r < 9; r++) {
+            build.append("|\t");
+            for (int c = 0; c < 9; c++) {
+                build.append(board[r][c] == 0 ? "." : board[r][c]).append("|\t");
+            }
+            build.append("|\n");
+        }
+        build.append(" -----------------\n");
+        return build.toString();
+    }
+
+    public boolean isValid() {
+        return isValidData() && rowsValid() && columnsValid() && miniSquaresValid();
+    }
+
+    private boolean isValidData() {
         for (int[] row : board) {
             for (int val : row) {
                 if (val != 0 && (val < 1 || val > 9)) {
@@ -43,54 +53,68 @@ public class SudokuBoard {
         }
         return true;
     }
-   
-   private boolean rowsValid() {
+
+    private boolean rowsValid() {
         for (int r = 0; r < 9; r++) {
-            Set<Integer> a = new HashSet<>();
+            Set<Integer> seen = new HashSet<>();
             for (int c = 0; c < 9; c++) {
                 int val = board[r][c];
-                if (val != 0 && !a.add(val)) {
-                    return false;
+                if (val != 0) {
+                    if (seen.contains(val)) {
+                        return false;
+                    } else {
+                        seen.add(val);
+                    }
                 }
             }
         }
         return true;
     }
-   public boolean isSolve() {
-   
-   }
-   
-   public String toString() {
-      String build = " -----------------\n";
-      for(int r = 0; r < board.length; r++) {
-         build += "|\t";
-         for(int c = 0; c < board[0].length; c++) {
-            build += board[r][c] + "|\t";
-         }
-         build += "|\n";
-      }
-      build += " -----------------\n";
-      return build;   
-   }
-}
 
-/*
-# PROGRAM OUTPUT
+    private boolean columnsValid() {
+        for (int c = 0; c < 9; c++) {
+            Set<Integer> seen = new HashSet<>();
+            for (int r = 0; r < 9; r++) {
+                int val = board[r][c];
+                if (val != 0) {
+                    if (seen.contains(val)) {
+                        return false;
+                    } else {
+                        seen.add(val);
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
-  ----jGRASP exec: java SudokuEngine
-  -----------------
- |	50|	0|	0|	49|	0|	53|	0|	0|	51|	|
- |	0|	53|	52|	0|	0|	0|	55|	49|	0|	|
- |	0|	49|	0|	50|	0|	51|	0|	56|	0|	|
- |	54|	0|	50|	56|	0|	55|	51|	0|	52|	|
- |	0|	0|	0|	0|	0|	0|	0|	0|	0|	|
- |	49|	0|	53|	51|	0|	57|	56|	0|	54|	|
- |	0|	50|	0|	55|	0|	49|	0|	54|	0|	|
- |	0|	56|	49|	0|	0|	0|	50|	52|	0|	|
- |	55|	0|	0|	52|	0|	50|	0|	0|	49|	|
-  -----------------
- 
- 
-  ----jGRASP: Operation complete.
+    private boolean miniSquaresValid() {
+        for (int i = 1; i <= 9; i++) {
+            int[][] mini = miniSquare(i);
+            Set<Integer> seen = new HashSet<>();
+            for (int[] row : mini) {
+                for (int val : row) {
+                    if (val != 0) {
+                        if (seen.contains(val)) {
+                            return false;
+                        } else {
+                            seen.add(val);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
 
-*/
+    private int[][] miniSquare(int spot) {
+        int[][] mini = new int[3][3];
+        for (int r = 0; r < 3; r++) {
+            for (int c = 0; c < 3; c++) {
+                mini[r][c] = board[(spot - 1) / 3 * 3 + r][(spot - 1) % 3 * 3 + c];
+            }
+        }
+        return mini;
+    }
+
+ }   
